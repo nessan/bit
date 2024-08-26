d473871630d7b84215ad4e772b95d5eff1862b08
nessan <nessan.fitzmaurice@me.com>
2024-08-26 13:04:08 +0100

    link fix

--- examples/scratch01.cpp
+++ examples/scratch01.cpp
@@ -1,56 +1,23 @@
-#include "common.h"
-int
-main()
+#include <bit/bit.h>
+int main()
 {
-    auto v = bit::vector<std::uint8_t>::ones(77);
+    std::size_t m = 12;
 
-    std::cout << std::format("bit::vector: {:b}\n", v);
-    std::cout << "Exporting that bit-vector to words of various types:\n";
-    uint16_t word16;
-    v.export_bits(word16);
-    std::cout << std::format("uint16_t:    {}\n", word16);
-    uint32_t word32;
-    v.export_bits(word32);
-    std::cout << std::format("uint32_t:    {}\n", word32);
-    uint64_t word64;
-    v.export_bits(word64);
-    std::cout << std::format("uint64_t:    {}\n", word64);
-    std::cout << std::endl;
+    auto A = bit::matrix<>::random(m);
+    auto b = bit::vector<>::random(m);
+    auto x = bit::solve(A, b);
 
-    std::cout << std::format("bit::vector: {:b}\n", v);
-    std::cout << "Exporting that bit-vector to a std::array of words of various types:\n";
-    constexpr std::size_t   N = 4;
-    std::array<uint16_t, N> arr16;
-    v.export_bits(arr16);
-    std::cout << std::format("std::array<uint16_t,4>: {}\n", arr16);
-    std::array<uint32_t, N> arr32;
-    v.export_bits(arr32);
-    std::cout << std::format("std::array<uint32_t,4>: {}\n", arr32);
-    std::array<uint64_t, N> arr64;
-    v.export_bits(arr64);
-    std::cout << std::format("std::array<uint64_t,4>: {}\n", arr64);
-    std::cout << std::endl;
-
-    std::cout << std::format("bit::vector: {:b}\n", v);
-    std::cout << "Exporting that bit-vector to a std::vector of words of various types:\n";
-    std::vector<uint16_t> vec16(N);
-    v.export_bits(vec16);
-    std::cout << std::format("std::vector<uint16_t>: {}\n", vec16);
-    std::vector<uint32_t> vec32(N);
-    v.export_bits(vec32);
-    std::cout << std::format("std::vector<uint32_t>: {}\n", vec32);
-    std::vector<uint64_t> vec64(N);
-    v.export_bits(vec64);
-    std::cout << std::format("std::vector<uint64_t>: {}\n", vec64);
-    std::cout << std::endl;
-
-    std::cout << std::format("bit::vector: {:b}\n", v);
-    std::cout << "Exporting ALL of that bit-vector to a std::vector of words of various types:\n";
-    v.export_all_bits(vec16);
-    std::cout << std::format("std::vector<uint16_t>: {}\n", vec16);
-    v.export_all_bits(vec32);
-    std::cout << std::format("std::vector<uint32_t>: {}\n", vec32);
-    v.export_all_bits(vec64);
-    std::cout << std::format("std::vector<uint64_t>: {}\n", vec64);
-    std::cout << std::endl;
+    if (x) {
+        // Check that x is indeed a solution by computing A.x and comparing that to b
+        auto Ax = bit::dot(A, *x);
+        std::cout << "bit::matrix A, solution vector x, product A.x, and right hand side b:\n";
+        std::cout << "      A         x      A.x      b\n";
+        bit::print(A, *x, Ax, b);
+        std::cout << "So A.x == b? " << (Ax == b ? "YES" : "NO") << '\n';
+    }
+    else {
+        std::cout << "System A.x = b has NO solutions for A and b as follows:\n";
+        std::cout << "      A         x\n";
+        bit::print(A, b);
+    }
 }
